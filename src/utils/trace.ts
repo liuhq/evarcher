8f65c74e737f83cf7f8eeaa4a974cb290f47c5a1
Horace Liu <im.liuhq@gmail.com>
2025-12-03 18:57:13 +0800

    refactor(trace): new trace api

--- src/utils/trace.ts
+++ src/utils/trace.ts
@@ -1,21 +1,111 @@
-type Level = 'INFO' | 'WARN' | 'ERROR'
-export type Trace = (level: Level, message: string) => void
-export const createTrace = (enabled: boolean): Trace => {
+import { type DateTime, datetime } from './datetime'
+
+type Accessor<T> = () => T
+
+type TraceTemplateMeta = {
+    level: Level
+    datetime: Accessor<DateTime>
+}
+
+type TraceTemplateProps<S extends Record<string, unknown>> = {
+    meta: TraceTemplateMeta
+    slot: S
+}
+
+type TraceTemplate<S extends Record<string, unknown>> = (
+    props: TraceTemplateProps<S>,
+) => string
+
+type TracePrint<S extends Record<string, unknown>> = (slot: S) => void
+
+type Level = 'info' | 'warn' | 'error'
+
+type TraceLevel<S extends Record<string, unknown>> = {
+    [T in Level]: TracePrint<S>
+}
+
+const print_ = (level: Level, message: string) => {
     const report: { [L in Level]: (message: string) => void } = {
-        INFO: (msg) => {
+        info: (msg) => {
             console.info(msg)
         },
-        WARN: (msg) => {
+        warn: (msg) => {
             console.warn(msg)
         },
-        ERROR: (msg) => {
+        error: (msg) => {
             console.error(msg)
         },
     }
 
-    return (level: Level, message: string) => {
-        if (enabled) {
-            report[level](message)
+    report[level](message)
+}
+
+type DefaultSlot = { message: string }
+const default_template: TraceTemplate<DefaultSlot> = (props) => {
+    return props.slot.message
+}
+
+export type Trace<S extends Record<string, unknown> = DefaultSlot> = (
+    template?: TraceTemplate<S>,
+) => TraceLevel<S>
+
+export const createTrace = <S extends Record<string, unknown> = DefaultSlot>(
+    enabled: boolean,
+) => {
+    const enable = (callback: () => void) => {
+        if (!enabled) return
+        callback()
+    }
+
+    const meta: TraceTemplateMeta = {
+        level: 'info',
+        datetime,
+    }
+
+    return <RS extends Record<string, unknown> = S>(
+        template?: TraceTemplate<RS>,
+    ): TraceLevel<RS> => {
+        const tmpl = (template ?? default_template) as TraceTemplate<RS>
+
+        const info: TraceLevel<RS>['info'] = (slot) => {
+            const level = 'info'
+            enable(() =>
+                print_(
+                    level,
+                    tmpl({
+                        meta,
+                        slot,
+                    }),
+                )
+            )
         }
+
+        const warn: TraceLevel<RS>['warn'] = (slot) => {
+            const level = 'warn'
+            enable(() =>
+                print_(
+                    level,
+                    tmpl({
+                        meta: { ...meta, level },
+                        slot,
+                    }),
+                )
+            )
+        }
+
+        const error: TraceLevel<RS>['error'] = (slot) => {
+            const level = 'error'
+            enable(() =>
+                print_(
+                    level,
+                    tmpl({
+                        meta: { ...meta, level },
+                        slot,
+                    }),
+                )
+            )
+        }
+
+        return { info, warn, error }
     }
 }
